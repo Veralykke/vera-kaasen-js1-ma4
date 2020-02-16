7602bd04fcd104ce3d25f2e154401a9caea9ee41
Vera Lykke Kaasen <55652971+Veralykke@users.noreply.github.com>
2020-02-16 19:24:08 +0100

    fixt some buuugs

--- js3/script3.js
+++ js3/script3.js
@@ -12,6 +12,7 @@ fetch(games4200Url)
     });
 
 function createGameDetails(json) {
+
     const h1 = document.querySelector("h1");
     h1.innerHTML = json.name;
 
@@ -24,68 +25,3 @@ function createGameDetails(json) {
     const description = document.querySelector(".description");
     description.innerHTML = json.description;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////
-
-/*const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-
-let id;
-
-if (params.has("id")) {
-    id = params.get("id");
-} else {
-    document.location.href = "creators.html";
-}
-
-const creatorsUrl = `https://via.placeholder.com/1000`;
-const detailUrl = `${creatorsUrl}${id}`;
-
-fetch(detailUrl)
-    .then(function)(response) {
-        return response.json();
-    })
-.then(function(json) {
-        createCreator(json);
-    })
-    .catch(function() {
-        document.location.href = "error.html";
-    });
-
-function createCreator(json) {
-    console.dir(json);
-
-    const image = document.querySelector(".creator-image");
-    image.src = json.image;
-    image.alt = json.name;
-
-    const name = document.querySelector(".creator-name");
-    name.innerHTML = json.name;
-
-    const description = document.querySelector(".creator-description");
-    description.innerHTML = json.description;
-}*/
